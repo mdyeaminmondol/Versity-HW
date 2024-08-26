0b92902fdd9ac50ab784d4ac3931e3b61b13cc00
mdyeaminmondo <mdyeaminmondol538@gmail.com>
2024-08-26 23:46:24 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 13, b = 13;
+        int result1, result2;
+        System.out.println("Value of a: " + a);
+
+        result1 = ++a;
+        System.out.println("After increment: " + result1);
+
+        System.out.println("Value of b: " + b);
+        result2 = --b;
+
+        System.out.println("After decrement: " + result2);
+
+    }
+}
